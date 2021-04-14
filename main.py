4316dea859cd5a4485aa5ee2d7c4025fe33e9eab
Jakub <jakubhryniewicki21@gmail.com>
2021-04-14 18:53:41 +0200

    Initial commit

---
+++ main.py
@@ -0,0 +1,38 @@
+import numpy as num
+# ZAD 1
+# krotnosc = num.arange(4,20*4+4,4)
+# print(krotnosc)
+
+# ZAD 2
+# polecenie = num.arange(0, 0.2, 0.01)
+# print(polecenie)
+# ewolucja = polecenie.astype('int32')
+# print((ewolucja))
+# print(ewolucja.dtype)
+
+# Zad 3
+# def funkcja(n):
+#     a = num.empty((n,n))
+#     a = a.astype('int')
+#     b = 0
+#     for i in range(n):
+#         for j in range(n):
+#             a[i,j] = (2**b)
+#             b += 1
+#     print(a)
+#
+#
+#
+# n = input("PODAJ liczbe: ")
+# n = int(n)
+#
+# print(funkcja(n))
+
+# Zad 6
+
+# wykreslanka = num.array([['K', 'U', 'R', 'C', 'Z', 'E'], ['R', 'O', 'B', 'D', 'F', 'W'], ['Ó', 'Z', 'L', 'N', 'Q', 'V'],
+#                         ['W', 'A', 'U', 'A', 'T', 'M'], ['K', 'E', 'C', 'S', 'N', 'Z'], ['A', 'K', 'T', 'S','O', 'O']
+#                         ])
+#
+# print(wykreslanka)
+
